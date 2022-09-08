cee39f09dfa8eb7ee1b5c2ef58744ff72a3999a2
leilani-heather <leilaniheather@gmail.com>
2022-09-08 19:46:20 +1200

    Partial tailwind CLI install. Images added. Readme WIP

--- client/components/__tests__/App.test.js
+++ client/components/__tests__/App.test.js
@@ -2,8 +2,8 @@ import React from 'react'
 import { Provider } from 'react-redux'
 import { screen, render } from '@testing-library/react'
 
-import App from './App'
-import store from '../store'
+import App from '../App'
+import store from '../../store'
 // import { fetchFruits } from '../actions'
 
 // jest.mock('../actions')
