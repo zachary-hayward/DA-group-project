db98b1b6b4da37a8932f88a18e1d869f12e0c531
leilani-heather <leilaniheather@gmail.com>
2022-09-09 12:54:31 +1200

    Tailwind experiment

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,17 +1,11 @@
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-
+import React from 'react'
+import MockHomepage from './MockHomepage'
 
 function App() {
  
   return (
     <>
-      <div className="app">
-        <h1 className="bg-lime-600">Kes-Ke-Say</h1>
-        <h2>This is a test</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ex velit, varius vel augue ac, bibendum iaculis ante. Vivamus feugiat scelerisque nulla vitae aliquet. Fusce quis ultrices est, sit amet luctus nunc. Ut varius sed lorem fringilla efficitur. Curabitur quam risus, interdum sed neque vel, laoreet fermentum mi. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Duis quis posuere sapien. Donec nec elementum metus, quis placerat massa. Nunc in bibendum tellus. Integer vel purus dui. Morbi et ullamcorper eros. Etiam id dolor lacus.</p>
-        <button className="btn-blue">This is a test too</button>
-      </div>
+      <MockHomepage />
     </>
   )
 }
