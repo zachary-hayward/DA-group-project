2043f67956fdd1b5931700b8dff191a66e57d83f
Jatin-Puri <jatinpuri11@gmail.com>
2022-09-13 14:00:31 +1200

    change userid to auth0id

--- server/db/seeds/group_members.js
+++ server/db/seeds/group_members.js
@@ -6,12 +6,12 @@ exports.seed = async function(knex) {
   // Deletes ALL existing entries
   await knex('group_members').del()
   await knex('group_members').insert([
-    {id: 1, user_id: 1, group_id: 1},
-    {id: 2, user_id: 3, group_id: 1},
-    {id: 3, user_id: 2,  group_id: 2},
-    {id: 4, user_id: 3,  group_id: 2},
-    {id: 5, user_id: 4,  group_id: 2},
-    {id: 6, user_id: 1,  group_id: 3},
-    {id: 7, user_id: 2,  group_id: 3}
+    {id: 1, auth0_id: 'auth0|123', group_id: 1},
+    {id: 2, auth0_id: 'auth0|345', group_id: 1},
+    {id: 3, auth0_id: 'auth0|234',  group_id: 2},
+    {id: 4, auth0_id: 'auth0|345',  group_id: 2},
+    {id: 5, auth0_id: 'auth0|456',  group_id: 2},
+    {id: 6, auth0_id: 'auth0|123',  group_id: 3},
+    {id: 7, auth0_id: 'auth0|234',  group_id: 3}
   ]);
 };
