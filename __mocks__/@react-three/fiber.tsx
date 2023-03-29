98cf03aeca2881355213b99fc5768dede1466fb5
Gerard Paapu <gerard@gerardpaapu.com>
2023-03-29 16:09:22 +1300

    test: add mocks for the @react-three libraries

---
+++ __mocks__/@react-three/fiber.tsx
@@ -0,0 +1,9 @@
+// import { Canvas, useThree, useFrame } from '@react-three/fiber'
+
+export function Canvas() {
+  return null
+}
+
+export function useThree() {
+  return {}
+}
---
+++ __mocks__/@react-three/postprocessing.tsx
@@ -0,0 +1,9 @@
+// import { EffectComposer, DepthOfField } from '@react-three/postprocessing'
+
+export function EffectComposer() {
+  return null
+}
+
+export function DepthOfField() {
+  return null
+}
