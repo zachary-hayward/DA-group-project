1571281146e026171c41ea8cde32c03a53385836
krissy-legeyt <krissylegeyt@gmail.com>
2023-03-30 15:55:32 +1300

    change import to legacy_createStore

--- client/store.ts
+++ client/store.ts
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware } from 'redux'
+import { legacy_createStore as createStore, applyMiddleware } from 'redux'
 import thunkMiddleware from 'redux-thunk'
 import { composeWithDevTools } from '@redux-devtools/extension'
 import type { ThunkAction as BaseThunkAction } from 'redux-thunk'
