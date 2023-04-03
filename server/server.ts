7b5a02d513f6346a35b3987637f77506ba801625
krissy-legeyt <krissylegeyt@gmail.com>
2023-04-03 19:11:41 +1200

    remove home route

--- server/server.ts
+++ server/server.ts
@@ -1,7 +1,6 @@
 import express from 'express'
 import { join } from 'node:path'
 
-import homeRoutes from './routes/home'
 import usersRoutes from './routes/users'
 import postsRoutes from './routes/posts'
 import groupsRoutes from './routes/groups'
@@ -11,7 +10,6 @@ const server = express()
 server.use(express.json())
 server.use(express.static(join(__dirname, 'public')))
 
-server.use('/api/v1/home', homeRoutes)
 server.use('/api/v1/users', usersRoutes)
 server.use('/api/v1/posts', postsRoutes)
 server.use('/api/v1/groups', groupsRoutes)
