c864bdd66bbbff1c7dbfea0c878cbf61293a68c4
Jatin-Puri <jatinpuri11@gmail.com>
2023-06-18 11:31:02 +1200

    fix css error

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   test: {
+    // exclude: ['**/__mocks__/*.js'],
     deps: {
-      inline: ['three'],
+      inline: ['three-stdlib'],
     },
   },
   plugins: [react()],
