6ba4f9708131e47a7b9b167bd1cf8dc54cdd4960
Gerard Paapu <gerard@gerardpaapu.com>
2023-06-18 11:45:13 +1200

    test: mock out all the danger libraries for tests

--- client/test-utils.ts
+++ client/test-utils.ts
@@ -1,4 +1,4 @@
-import { beforeEach, expect } from 'vitest'
+import { beforeEach, expect, vi } from 'vitest'
 import { cleanup } from '@testing-library/react'
 import matchers, {
   TestingLibraryMatchers,
@@ -6,6 +6,10 @@ import matchers, {
 
 beforeEach(cleanup)
 expect.extend(matchers)
+
+vi.mock('@react-three/drei')
+vi.mock('@react-three/fiber')
+vi.mock('@react-three/postprocessing')
 
 declare global {
   // eslint-disable-next-line @typescript-eslint/no-namespace
