025223bde67e7d9da916fd5c3b796608ebbdef05
krissy-legeyt <krissylegeyt@gmail.com>
2023-06-19 21:47:53 +1200

    test: update LandingPage to route render

--- client/components/__tests__/LandingPage.test.tsx
+++ client/components/__tests__/LandingPage.test.tsx
@@ -1,18 +1,13 @@
 // @vitest-environment jsdom
 import { describe, it, expect } from 'vitest'
-import '../../test-utils'
-import { screen, render } from '@testing-library/react'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { screen } from '@testing-library/react'
 
-import LandingPage from '../LandingPage'
+import { renderRoute } from '../../test-utils'
 
 describe('<LandingPage />', () => {
   it('renders a button that has a <button> html tag and a class name', () => {
-    render(
-      <Router>
-        <LandingPage />
-      </Router>
-    )
+    renderRoute('/login')
+
     const button = screen.getByRole('button', { name: /Sign Up/ })
     expect(button).toContainHTML('button')
     expect(button).toHaveClass('px-8')
