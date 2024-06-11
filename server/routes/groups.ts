271a6e29a4de0caa8babd7e03adb470d087e7867
Eli Cantwell <163774606+eli-cantwell@users.noreply.github.com>
2024-06-11 15:09:50 +1200

    commit express route

--- server/routes/groups.ts
+++ server/routes/groups.ts
@@ -1,10 +1,17 @@
 import express from 'express'
-
+import * as db from '../db/functions/groups.ts'
 const router = express.Router()
 
 // GET /api/v1/groups
-router.get('/', (req, res) => {
-  res.status(200).send('Hello from the groups route!')
+router.get('/', (req, res, next) => {
+  try {
+    const groups = db.getAllGroups()
+    res.json(groups)
+
+    res.status(200).send('Hello from the groups route!')
+  } catch (e) {
+    next(e)
+  }
 })
 
 export default router
