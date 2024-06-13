70bba828b4d7a954a923e0690db94f4cf2daedf4
Zachary Hayward <zmhhayward@gmail.com>
2024-06-12 21:18:00 -0500

    user homepage initial css

--- client/components/Home.tsx
+++ client/components/Home.tsx
@@ -16,10 +16,20 @@ export default function Home() {
       <div className='relative flex flex-grow justify-center'>
         <div className='absolute inset-0 z-0 bg-kks-wine w-full'></div>
         <div className='absolute inset-0 z-0 bg-kks-blue w-1/2'></div>
-        <div className="relative container z-10 bg-white flex flex-col space-y-2 sm:space-y-4 lg:space-y-8 xl:space-y-12 w-full sm:max-w-[640px] border border-1 border-black">
-          <div className="flex flex-row-reverse">
-            <button className="btn-blue bg-kks-blue m-2 ">View All Profiles</button>
+        <div className="relative container z-10 bg-white flex flex-col w-full sm:max-w-[640px] md:max-w-[800px] lg:max-w-[960px] border border-1 border-black">
+          <div className='absolute inset-0 z-0 bg-kks-wine w-full sm:hidden'></div>
+          <div className='absolute inset-0 z-0 bg-white w-2/3 sm:hidden'></div>
+          <div className='absolute inset-0 z-0 bg-kks-blue w-1/3 sm:hidden'></div>
+          <div className='flex flex-grow flex-col z-10'>
+            <div className="flex flex-row-reverse">
+              <button className="btn-blue bg-kks-blue m-2 scale-75 sm:scale-100">Post</button>
+            </div>
+            <div className='flex flex-grow border sm:border-1 md:border-2 border-black w-4/5 max-w-[800px] mx-auto bg-white'>
+              
+            </div>
+
           </div>
+          <br/>
         </div>
       </div>
     </>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,16 @@ module.exports = {
   content: ['./index.html', './client/**/*.{js,ts,jsx,tsx}'],
 
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        kks: {
+          black: '#333333',
+          grey: '#dcd8d6',
+          blue: '#364da1',
+          wine: '#4f0404'
+        }
+      }
+    },
   },
   plugins: [],
 }
