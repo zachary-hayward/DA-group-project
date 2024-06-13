0f0efcc307d95562fb9c1b9c3a0f0879dfefcec9
Zachary Hayward <zmhhayward@gmail.com>
2024-06-12 20:25:26 -0500

    basic css setup

--- client/components/Home.tsx
+++ client/components/Home.tsx
@@ -11,5 +11,17 @@ export default function Home() {
     }
   }, [isAuth, navigate])
 
-  return <div>Home</div>
+  return (
+    <>    
+      <div className='relative flex flex-grow justify-center'>
+        <div className='absolute inset-0 z-0 bg-kks-wine w-full'></div>
+        <div className='absolute inset-0 z-0 bg-kks-blue w-1/2'></div>
+        <div className="relative container z-10 bg-white flex flex-col space-y-2 sm:space-y-4 lg:space-y-8 xl:space-y-12 w-full sm:max-w-[640px] border border-1 border-black">
+          <div className="flex flex-row-reverse">
+            <button className="btn-blue bg-kks-blue m-2 ">View All Profiles</button>
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
